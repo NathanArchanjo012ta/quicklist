1aa7f26bd226f2f088a6aa79c13ec6876f2d97cf
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-05-31 20:47:38 -0300

    criação do aquivo main .js, array de itense da função que adiciona itens ao array

---
+++ main.js
@@ -0,0 +1,17 @@
+const items =[]
+
+function addItem(){
+    const itemName = document.querySelector("#item").value
+
+    const item = {
+        name: itemName;
+        checked : false
+    }
+
+    items.push
+
+    document.querySelector("#item").value = ""
+
+    console.log(items)
+}
+ 
