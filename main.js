a3d570c934eaf22bcc57343f4a88f900a7cfea74
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-06-01 17:33:30 -0300

    criação da função checkItem para marcar e desmarcar itens

--- main.js
+++ main.js
@@ -27,11 +27,11 @@ function showitemsList() {
     sectionList.innerHTML += `
       <div class="item">
         <div>
-          <input type="checkbox" name="list" id="item-${index}">
-          <div class="custom-checkbox">
+          <input type="checkbox" name="list" id="item-${index}"  ${item.checked && "checked"}>
+          <div class="custom-checkbox" onclick = "checkItem('${item.name}')">
             <img src="./assets/checked.svg" alt="checked">
           </div>
-          <label for="item-${index}">${item.name}</label>
+          <label for="item-${index}" onclick = "checkItem('${item.name}')" >${item.name}</label>
         </div>
         <button onclick="removeItem('${item.name}')">
           <img src="./assets/trash-icon.svg" alt="trash icon">
@@ -39,6 +39,13 @@ function showitemsList() {
       </div>
     `
   })
+}
+
+function checkItem(itemName){
+  const item = items.find((item) =>item.name === itemName)
+ item.checked = !item.checked
+ showitemsList()
+
 }
 
 
