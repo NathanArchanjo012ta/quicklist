d548f850759b52b70cb7e5cdf1f065f607c7b1e9
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-06-01 17:12:30 -0300

    criação da funcção para remover os itens do array

--- main.js
+++ main.js
@@ -1,42 +1,56 @@
-const items =[]
+const items = []
 
-function addItem(){
-    const itemName = document.querySelector("#item").value
+function addItem() {
+  const itemName = document.querySelector("#item").value
 
-    const item = {
-        name: itemName;
-        checked : false
-    }
+  if (itemName.trim() === "") return // impede adicionar item vazio
 
-    items.push
+  const item = {
+    name: itemName,
+    checked: false
+  }
 
-    document.querySelector("#item").value = ""
+  items.push(item)
 
-    showitemsList(item)
+  document.querySelector("#item").value = ""
 
-    console.log(items)
+  showitemsList()
+
+  console.log(items)
 }
- 
-function showitemsList(){
-    const sectionList = document.querySelector(".list")
 
+function showitemsList() {
+  const sectionList = document.querySelector(".list")
+  sectionList.innerHTML = "" // limpa antes de reescrever
 
-    items.map((item, index)=>{
-        sectionList.innerHTML += '<div class="item">
-          <div>
-            <input type="checkbox" name="list" id="item-${index}">
-            <div class="custom-checkbox">
-              <img src="./assets/checked.svg" alt="checked">
-            </div>
-            <label for="item-1">${item.name}</label>
+  items.map((item, index) => {
+    sectionList.innerHTML += `
+      <div class="item">
+        <div>
+          <input type="checkbox" name="list" id="item-${index}">
+          <div class="custom-checkbox">
+            <img src="./assets/checked.svg" alt="checked">
           </div>
-          <button>
-            <img src="./assets/trash-icon.svg" alt="trash icon">
-          </button>
+          <label for="item-${index}">${item.name}</label>
         </div>
-        '
-
-
-    })
+        <button onclick="removeItem('${item.name}')">
+          <img src="./assets/trash-icon.svg" alt="trash icon">
+        </button>
+      </div>
+    `
+  })
 }
-{}
+
+
+function removeItem(itemName){
+  const itemIndex = items.findIndex((item) => item.name === itemName)
+  const divwarning = document.querySelector(".warning")
+
+  divwarning.classList.remove("hide-warning")
+
+
+  if(itemIndex !== -1){
+    items.splice(itemIndex, 1)
+  }
+  showitemsList()
+}
