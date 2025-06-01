8bc103783b213a562f176a18a1b3bc2cf5391ba6
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-06-01 17:48:20 -0300

    adição do array de items e verifylocalstorageItems

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = []
+let items = []
 
 function addItem() {
   const itemName = document.querySelector("#item").value
@@ -44,6 +44,7 @@ function showitemsList() {
     `
   })
 }
+localStorage.setItem("items", JSON.stringify(items))
 
 function checkItem(itemName){
   const item = items.find((item) =>item.name === itemName)
@@ -72,3 +73,16 @@ function removeItem(itemName){
 function addHideWarningClass(){
   document.querySelector(".warning").classList.add("hide-warning")
 }
+
+
+function verifyLocalStorageItems() {
+  const localStorageItems = localStorage.getItem("items")
+
+  if (localStorageItems) {
+  items = JSON.parse(localStorageItems)
+  showitemsList()
+}
+
+}
+verifyLocalStorageItems()
+
